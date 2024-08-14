be3b385e5bf480db09f54e4c106fcc516eee1c0b
sejalbhaiswar <146980729+sejalbhaiswar@users.noreply.github.com>
2024-08-14 23:11:35 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-reportWebVitals();
+export { default as ChannelCard } from "./ChannelCard";
+export { default as VideoCard } from "./VideoCard";
+export { default as ChannelDetail } from "./ChannelDetail";
+export { default as Feed } from "./Feed";
+export { default as Loader } from "./Loader";
+export { default as Navbar } from "./Navbar";
+export { default as SearchBar } from "./SearchBar";
+export { default as VideoDetail } from "./VideoDetail";
+export { default as SearchFeed } from "./SearchFeed";
+export { default as Videos } from "./Videos";
+export { default as Sidebar } from "./Sidebar";
