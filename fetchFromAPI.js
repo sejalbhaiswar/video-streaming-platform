e6931efbeb34995866cddf3f53508b0b7606dd26
sejalbhaiswar <146980729+sejalbhaiswar@users.noreply.github.com>
2024-08-14 23:12:57 +0530

    Add files via upload

---
+++ fetchFromAPI.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    "X-RapidAPI-Key": "e73421b725msh5e07492fdba12c4p1872a8jsn27c1c1cec87e",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+};
